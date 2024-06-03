01d00626543a4320d3eb029dc9d983cb5ee8db11
mspsitedeveloper <ahmadmahdavi188@yahoo.com>
2024-06-03 16:42:20 +0330

    edit front files

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('acounts/', include('django.contrib.auth.urls')),
     path('', include('post.urls'))
 ]
